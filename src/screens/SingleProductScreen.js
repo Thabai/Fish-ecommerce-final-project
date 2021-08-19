ca0162bac97a4c2386b4b954808d04fc615da2bf
Mikey Cleworth <81911172+mikey-cle@users.noreply.github.com>
2021-08-19 16:21:36 +0100

    single product page done

--- src/screens/SingleProductScreen.js
+++ src/screens/SingleProductScreen.js
@@ -1,25 +1,34 @@
 import './SingleProductScreen.css';
 import { Route } from "react-router-dom";
+import { Link } from 'react-router-dom';
+
+
+
 
 const ProductScreen = (fish) => {
     return (
+      <div className='wrapper'>
       <div className="singleproductscreen">
-        <h1>Name: {fish.value.name}</h1>
-        <h2>Scientific Name: {fish.value.scientific}</h2>
-        <img src={fish.value.img} alt="Fish Image" />
-        <p>About: {fish.value.description}</p>
-        <h2>Habitat: {fish.value.habitat}</h2>
-        <h3>Price: {fish.value.price}</h3>
-        <h3>Stock: {fish.value.quantity}</h3>
-        <div>
-          <h2>Compatibility: </h2>
-          <h2>Temperature: {fish.value.compatibility.temperature}</h2>
-          <h2>Temperament: {fish.value.compatibility.temperament}</h2>
-          <h2>Food Source: {fish.value.compatibility.foodsource}</h2>
-          <h3>Social: {fish.value.compatibility.social}</h3>
-          <h3>Breeding: {fish.value.compatibility.breeding}</h3>
-        </div>
+        <h3 className='singleProdH'>Name: <span className='bright'>{fish.value.name}</span></h3>
+        <h3 className='singleProdH'>Scientific Name: <span className='bright'> {fish.value.scientific}</span></h3>
+        <img className='fishImage' src={fish.value.img} alt="Fish Image" />
+        <p><span className='singleProdH'>About:</span> {fish.value.description}</p>
+        <h3 className='singleProdH'>Habitat: <span className='bright'>{fish.value.habitat}</span></h3>
+        <h3 className='singleProdH'>Price: <span className='bright'>£{fish.value.price}</span></h3>
+        <h3 className='singleProdH'>Stock: <span className='bright'>{fish.value.quantity}</span></h3>
+     
+      </div> 
+        <div className='compatInfo'>
+          <h3 className='singleProdH titleBox'>Compatibility </h3>
+          <h3 className='singleProdH'>Temperature:<span className='bright'> {fish.value.compatibility.temperature}</span></h3>
+          <h3 className='singleProdH'>Temperament: <span className='bright'>{fish.value.compatibility.temperament}</span></h3>
+          <h3 className='singleProdH'>Food Source: <span className='bright'>{fish.value.compatibility.foodsource}</span></h3>
+          <h3 className='singleProdH'>Social:<span className='bright'>{fish.value.compatibility.social}</span> </h3>
+          <h3 className='singleProdH'>Breeding: <span className='bright'>{fish.value.compatibility.breeding}</span></h3>
+          {/* <button className="cart__button" onClick={() => onAdd(data)}>Add to cart</button> */}
+   
       </div>
+        </div>
     );
 };
 
